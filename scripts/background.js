4d165f80e3868ed94bc2093373059ca33ffe20b3
Anton <anton9121@gmail.com>
2013-09-10 16:30:40 +0600

    wm upd

--- scripts/background.js
+++ scripts/background.js
@@ -24,7 +24,39 @@ var wm = function() {
 			}
 		});
 	};
-	var check = function() {
+	var create_playlist_window = function(wm_id) {
+		wm_id = check(wm_id);
+		if (app_windows[wm_id] === undefined || app_windows[wm_id].player === null) {
+			return;
+		}
+		if (app_windows[wm_id].playlist !== null) {
+			app_windows[wm_id].playlist.focus();
+			return;
+		}
+		var create_window = function(p_l, p_t) {
+			chrome.app.window.create('index.html', {
+				bounds: {
+					width: 335,
+					height: 400,
+					left: p_l,
+					top: p_t
+				},
+				frame: "none",
+				resizable: false
+			}, function(win) {
+				win.contentWindow.window.wm_id = wm_id;
+				app_windows[wm_id].playlist = win;
+			});
+		};
+		chrome.storage.local.get(function(storage) {
+			if ('pl_pos_left' in storage && 'pl_pos_top' in storage) {
+				create_window(storage.pl_pos_left, storage.pl_pos_top);
+			} else {
+				create_window(30, 30);
+			}
+		});
+	};
+	var check = function(wm_id) {
 		app_windows.forEach(function(item) {
 			if (item.player !== null && item.player.contentWindow.window === null) {
 				item.player = null;
@@ -53,6 +85,19 @@ var wm = function() {
 				count--;
 			}
 		}
+		for (var i = 0; i < app_windows.length; i++) {
+			var item = app_windows[i];
+			if (item.player !== null) {
+				if (wm_id !== undefined && wm_id === item.player.contentWindow.window.wm_id) {
+					wm_id = i;
+				}
+				item.player.contentWindow.window.wm_id = i;
+			}
+			if (item.playlist !== null) {
+				item.playlist.contentWindow.window.wm_id = i;
+			}
+		}
+		return wm_id;
 	};
 	return {
 		get_player: function() {
@@ -70,7 +115,8 @@ var wm = function() {
 				});
 			}
 		},
-		create_player: create_player_window
+		create_player: create_player_window,
+		create_playlist: create_playlist_window
 	};
 }();
 chrome.app.runtime.onLaunched.addListener(function() {
